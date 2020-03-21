6a56b831802c2a4f3215df6e2c6c413518ea184a
Luis Villalobos <luisevm24@outlook.com>
2020-03-21 01:45:09 -0600

    update: concanation & interpolation

--- Basics/Stringss.php
+++ Basics/Stringss.php
@@ -29,4 +29,10 @@ NEWDOC TEST
 Lorem ipsum do reprehenderit laborum irure et sit aliqua deserunt nulla consequat dolore $breakLine incididunt qui id anim aliquip eiusmod laborum sunt aute laboris consectetur reprehenderit incididunt pariatur et ullamco qui sit velit et \t quis minim sint dolor dolor pariatur culpa magna anim occaecat duis pariatur officia id ex excepteur cupidatat aute in occaecat exercitation dolor culpa est ad officia ut irure esse et aliquip nulla ad cillum adipisicing voluptate ut dolor quis commodo do commodo sed consectetur dolore sit minim enim sed excepteur ullamco exercitation voluptate adipisicing exercitation deserunt nulla officia nulla velit pariatur laborum fugiat ut sunt excepteur do in velit laboris cupidatat tempor incididunt est sint cillum sint qui magna id laboris laboris aute consequat incididunt non ut pariatur ullamco in sit eiusmod sunt ad et laboris nisi aliqua mollit in eiusmod irure sit mollit labore sunt cillum amet minim in deserunt aliqua dolore cillum in dolor nostrud veniam dolor excepteur qui dolore. $dev
 NEWDOC_TEST;
 
+echo $breakLine.$breakLine;
+
+echo 'this is a example of concatanation ' .' by ' . $dev . $breakLine;
+echo "this is a example of interpolation by {$dev}" . $breakLine;
+
+
 ?>
