8c13375a26a68d8eece5cda7bad5e2291960fd66
Luis Villalobos <luisevm24@outlook.com>
2020-03-21 00:36:01 -0600

    learn: tags

---
+++ Basics/Tags.php
@@ -0,0 +1,5 @@
+/* <?php echo "The correct way to use the tag to run php";?> */
+/* <? echo "The alternative to use the tag in php";?> */
+/* <% echo "Deprecated" %> */
+/* <% echo "Deprecated" %> */
+/* <!--<script language="php">Deprecated</script>-->  */
