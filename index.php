254525230095758e02eaa0268cd0fec4c3271032
Luis Villalobos <luisevm24@outlook.com>
2020-03-21 00:35:40 -0600

    docs: create the index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>PHP Learning</title>
+</head>
+<body>
+	These are some of the resources I use in this repository <br/>
+	<ul>
+		<li><a href="https://codigofacilito.com/cursos/php-profesional">Código Facilito - Curso profesional de PHP</a></li>
+	</ul>
+</body>
+</html>
