0f8d5ff8463a8bfefc0c5628f0198374cc06b561
Luis Villalobos <luisevm24@outlook.com>
2020-03-22 20:32:17 -0600

    learn: switch

---
+++ ControlStructure/Switch.php
@@ -0,0 +1,21 @@
+<?php
+
+$variable = 1;
+switch ($variable) {
+	case 1:
+		echo 'Selected 1';
+	break;
+	case 2:
+		echo 'Selected 2';
+	break;
+
+	case 'Three':
+		echo 'Selected 3';
+	break;
+
+	default:
+		echo 'selected nothing';
+	break;
+}
+
+?>
