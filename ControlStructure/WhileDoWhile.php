1c7b23fd6101c5995a9e04206e52520b0184ac66
Luis Villalobos <luisevm24@outlook.com>
2020-03-22 20:33:38 -0600

    learn: do while & while

---
+++ ControlStructure/WhileDoWhile.php
@@ -0,0 +1,16 @@
+<?php
+
+$count = 0;
+
+while($count < 10){
+	echo "Iteration $count <br/>";
+	$count++;
+}
+
+$count = 9;
+do{
+	echo "first iteration, value: $count <br/>";
+	$count--;
+}while($count >= 9);
+
+ ?>
